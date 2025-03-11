8ae66e7dc09f4c2c71d698efe66dedcc1b96d23b
xiechao <xiechao2@tal.com>
2025-03-11 11:25:40 +0800

    fix bug

--- model/execute_command.py
+++ model/execute_command.py
@@ -262,7 +262,7 @@ class ExecuteCommand:
 
 				audio_data["continue"] = True # 设置标志位，打断以后，可以继续播放
 
-				self.audio_player.resume_interrupted(resp_msg_id, 1)
+				# self.audio_player.resume_interrupted(resp_msg_id, 1)
 				interrupt = self.audio_player.get_interrupt()
 				print(interrupt)
 				if interrupt is None:
