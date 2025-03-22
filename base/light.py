75abd9e91d01e161b3790ad30cf95dc4953c6664
xiechao <xiechao2@tal.com>
2025-03-22 09:39:27 +0800

    fix bug

--- base/light.py
+++ base/light.py
@@ -305,11 +305,11 @@ class Light:
                 pre_time = random.randint(0, 1000)
                 time.sleep(int(pre_time // 1000))
                 for j in range(rand_num_per_group):
-                    point_starts.append(random.randint(0, self.LED_COUNT))
+                    point_starts.append(random.randint(0, self.LED_COUNT - 1))
                     rgb1.append(fore_color)
                     rgb2.append(back_color)
                     nums.append(1)
-                threading.Thread(target=self.random_point_exec, args=([rgb1], [rgb2], [point_starts], nums, duration, pre_time)).start()
+                threading.Thread(target=self.random_point_exec, args=(rgb1, rgb2, [point_starts], nums, duration, pre_time)).start()
 
     def random_point_exec(self, rgb1, rgb2, point_starts, nums, duration = 1000, pre_time = 0):
         self.fade_total_by_range(rgb1, rgb2, point_starts, nums)
