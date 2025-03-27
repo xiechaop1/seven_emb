b7101b9b713e1e329391d63939cdb7a72f40d6cf
xiechao <xiechao2@tal.com>
2025-03-27 12:00:58 +0800

    fix bug

--- main.py
+++ main.py
@@ -108,7 +108,7 @@ if __name__ == "__main__":
     logging.info("audio is ready")
 
     screen_instance = Screen()
-    screen_instance.add("resources/video/think.mp4", 100)
+    screen_instance.add("resources/video/main.mp4", 100)
     screen_thread = threading.Thread(target=screen_instance.daemon)
     screen_thread.start()
     screen_instance.play()
