1a6e47b3678bda0ad2fb330898d892f269e68d31
xiechao <xiechao2@tal.com>
2025-04-02 18:20:27 +0800

    fix bug

--- base/motor.py
+++ base/motor.py
@@ -989,7 +989,10 @@ class Motor:
         # 将读取的值转换为电压值（范围0-4.096V）
         voltage = value * 4.096 / 32768.0
 
-        print("{:>5}it{:>5.3f}".format(self.chan.value, self.chan.voltage))
+        # print("{:>5}it{:>5.3f}".format(self.chan.value, self.chan.voltage))
+        diff = self.ads.read_adc_difference(2, gain=self.GAIN)
+
+        print("diff:", diff)
 
         return voltage
 
