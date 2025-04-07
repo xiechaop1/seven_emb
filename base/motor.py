029080a779d3c46d071c72a48e9cbd9f206f4028
xiechao <xiechao2@tal.com>
2025-04-07 09:49:45 +0800

    fix bug

--- base/motor.py
+++ base/motor.py
@@ -1217,7 +1217,7 @@ class Motor:
         global current
         with open("motor_degree.txt", "r") as md_file:
             tmp = md_file.read().strip()  # 读取并去除任何多余的空白字符
-            if tmp is not None:
+            if "," in tmp:
                 self.current_pos, self.current_pos2 = map(str, tmp.split(','))
             else:
                 self.current_pos, self.current_pos2 = 0, 0
