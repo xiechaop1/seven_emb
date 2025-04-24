011f4c0963ee11a430471396acb45350bc29ecd2
xiechao <xiechao2@tal.com>
2025-04-24 17:07:42 +0800

    fix bug

--- base/spray.py
+++ base/spray.py
@@ -60,9 +60,9 @@ class Spray:
         for i in range(times):
             for j in range(3):
                 GPIO.output(self.SPRAY_PIN, GPIO.HIGH)
-                time.sleep(1.5)
+                time.sleep(2)
                 GPIO.output(self.SPRAY_PIN, GPIO.LOW)
-                time.sleep(0.05)
+                time.sleep(0.25)
             print("GPIO.HIGH!")
             time.sleep(wait_time)
 
