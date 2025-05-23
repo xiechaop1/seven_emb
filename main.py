38b128efe793279977e80ac71d3bcac493db321c
xiechao <xiechao2@tal.com>
2025-05-23 09:49:32 +0800

    fix bug

--- main.py
+++ main.py
@@ -240,11 +240,11 @@ if __name__ == "__main__":
     #         on_release=on_release) as listener:
     #     listener.join()
     
-    # app = QApplication(sys.argv)
-    # window = gui.MainWindow()
-    # window.show()
-    #对接槽接口   
-    # comm.message.connect(window.messageHandler)
+    app = QApplication(sys.argv)
+    window = gui.MainWindow()
+    window.show()
+    # 对接槽接口
+    comm.message.connect(window.messageHandler)
     audio_play_thread.start()
     kaldi_thread.start()
     recv_thread.start()
